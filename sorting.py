87944b1743905e84f899476b3a23db57a95526c0
Marcus Kolbe <returnofthelast@gmail.com>
2020-04-29 09:43:09 +0200

    Reworked for PEP8

--- sorting.py
+++ sorting.py
@@ -2,16 +2,17 @@
 import random
 
 # Stalin Sort
-def StalinSort(args):
+def stalin_sort(args):
     i = 0
     while i < len(args) - 1:
-        if args[i] > args[i+1]: del args[i+1]
+        if args[i] > args[i+1]:
+            del args[i+1]
         else: i += 1
 
     return args
 
 # Bubble Sort
-def BubbleSort(args):
+def bubble_sort(args):
     switches = 1
     while switches > 0:
         switches = 0
@@ -23,46 +24,50 @@ def BubbleSort(args):
     return args
 
 # Quick Sort
-def QuickSort(args, minLen):
-    if len(args) < minLen:
-        return(BubbleSort(args))
+def quick_sort(args, min_len):
+    if len(args) < min_len:
+        return bubble_sort(args)
+
     else:
         part = random.randint(0, len(args) - 1)
-        splitPart1 = list()
-        splitPart2 = list()
+        split_part1 = list()
+        split_part2 = list()
 
         for i in range(0, len(args)):
             if i == part:
                 continue
-            elif args[i] < args[part]:
-                splitPart1.append(args[i])
-            else:
-                splitPart2.append(args[i])
-        
-        returnList = list()
-        returnList.extend(QuickSort(splitPart1, minLen))
-        returnList.append(args[part])
-        returnList.extend(QuickSort(splitPart2, minLen))
 
-    return returnList
+            elif args[i] < args[part]:
+                split_part1.append(args[i])
+            else:
+                split_part2.append(args[i])
+
+        return_list = list()
+        return_list.extend(quick_sort(split_part1, min_len))
+        return_list.append(args[part])
+        return_list.extend(quick_sort(split_part2, min_len))
+
+    return return_list
 
 # Bogo Sort
-def BogoSort(args):
+def bogo_sort(args):
     unsorted = 1
     while unsorted > 0:
         unsorted = 0
         random.shuffle(args)
 
         for i in range(0, len(args) - 1):
-            if args[i] > args[i+1]: unsorted += 1
+            if args[i] > args[i+1]:
+                unsorted += 1
 
     return args
 
-def InsertionSort(args):
+def insertion_sort(args):
     for i in range(1, len(args)):
         j = i
         for j in range(i, 0, -1):
-            if not (args[j - 1] > args[j]): break
+            if not args[j - 1] > args[j]:
+                break
             args[j], args[j - 1] = args[j - 1], args[j]
-    
+
     return args
---
+++ unit_tests.py
@@ -0,0 +1,78 @@
+# Import module
+import sorting
+
+# Stalin Sort tests
+STALIN_COUNT = 3
+STALIN_SUCCESS = 0
+
+if sorting.stalin_sort([5, 3, 55, 7, 9, 3, 2, 5, 2]) == [5, 55]:
+    STALIN_SUCCESS += 1
+
+if sorting.stalin_sort([6, 3, 22, 432, 666, 44, 9]) == [6, 22, 432, 666]:
+    STALIN_SUCCESS += 1
+
+if sorting.stalin_sort([8, 4, 22, 134, 88, 3, 999, 332, 445]) == [8, 22, 134, 999]:
+    STALIN_SUCCESS += 1
+
+print("Stalin Sort: " + str(STALIN_SUCCESS) + "/" + str(STALIN_COUNT))
+
+# Bubble Sort tests
+BUBBLE_COUNT = 3
+BUBBLE_SUCCESS = 0
+
+if sorting.bubble_sort([6, 4, 2, 11, 443, 554, 66]) == [2, 4, 6, 11, 66, 443, 554]:
+    BUBBLE_SUCCESS += 1
+
+if sorting.bubble_sort([7, 9, 3, 5, 23, 55, 34, 6]) == [3, 5, 6, 7, 9, 23, 34, 55]:
+    BUBBLE_SUCCESS += 1
+
+if sorting.bubble_sort([8, 3, 234, 554, 555, 6663, 2]) == [2, 3, 8, 234, 554, 555, 6663]:
+    BUBBLE_SUCCESS += 1
+
+print("Bubble Sort: " + str(BUBBLE_SUCCESS) + "/" + str(BUBBLE_COUNT))
+
+# Quick Sort tests
+QUICK_COUNT = 3
+QUICK_SUCCESS = 0
+MIN_LEN = 1
+
+if sorting.quick_sort([23, 55, 4, 33, 2, 66, 4], MIN_LEN) == [2, 4, 4, 23, 33, 55, 66]:
+    QUICK_SUCCESS += 1
+
+if sorting.quick_sort([5, 32, 55, 12, 44, 33, 86], MIN_LEN) == [5, 12, 32, 33, 44, 55, 86]:
+    QUICK_SUCCESS += 1
+
+if sorting.quick_sort([9, 3, 45, 23, 55, 432, 55], MIN_LEN) == [3, 9, 23, 45, 55, 55, 432]:
+    QUICK_SUCCESS += 1
+
+print("Quick Sort: " + str(QUICK_SUCCESS) + "/" + str(QUICK_COUNT))
+
+# Bogo Sort tests
+BOGO_COUNT = 3
+BOGO_SUCCESS = 0
+
+if sorting.bogo_sort([6, 8, 2, 13, 55, 332, 4]) == [2, 4, 6, 8, 13, 55, 332]:
+    BOGO_SUCCESS += 1
+
+if sorting.bogo_sort([6, 3, 2, 4, 932, 44, 32]) == [2, 3, 4, 6, 32, 44, 932]:
+    BOGO_SUCCESS += 1
+
+if sorting.bogo_sort([6, 5, 6, 33, 63, 33, 6, 44]) == [5, 6, 6, 6, 33, 33, 44, 63]:
+    BOGO_SUCCESS += 1
+
+print("Bogo Sort: " + str(BOGO_SUCCESS) + "/" + str(BOGO_COUNT))
+
+# Insertion Sort tests
+INSERTION_COUNT = 3
+INSERTION_SUCCESS = 0
+
+if sorting.insertion_sort([6, 3, 23, 55, 33, 2, 54]) == [2, 3, 6, 23, 33, 54, 55]:
+    INSERTION_SUCCESS += 1
+
+if sorting.insertion_sort([7, 32, 44, 554, 32, 6, 3]) == [3, 6, 7, 32, 32, 44, 554]:
+    INSERTION_SUCCESS += 1
+
+if sorting.insertion_sort([3, 53, 23, 5, 77, 54, 83]) == [3, 5, 23, 53, 54, 77, 83]:
+    INSERTION_SUCCESS += 1
+
+print("Insertion Sort: " + str(INSERTION_SUCCESS) + "/" + str(INSERTION_COUNT))
